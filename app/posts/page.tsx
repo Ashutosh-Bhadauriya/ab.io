e096e15b665edc73f4f6736a8b60c9fc555e1f01
AshutoshBhadauriya <ashutoshb455@gmail.com>
2023-05-18 22:12:18 +0530

    Feat: Add blog content

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,13 +2,20 @@ import { allPosts } from "@/.contentlayer/generated";
 import Link from "next/link";
 import { formatDate } from "@/lib/utils";
 import { merriweather_sans } from "@/lib/font";
+import { compareDesc } from "date-fns"
+
 
 export const metadata = {
   title: "Posts",
-  description: "All the blog posts"
-}
+  description: "All the blog posts",
+};
 
 export default function Posts() {
+  const posts = allPosts
+    .filter((post) => post.date)
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date));
+    });
   return (
     <div className="max-w-3xl mx-auto">
       <div className="mb-12">
@@ -23,7 +30,7 @@ export default function Posts() {
         </p>
       </div>
       <div className="border-l border-zinc-200 dark:border-zinc-800 pl-6">
-        {allPosts.map((post) => (
+        {posts.map((post) => (
           <article
             key={post._id}
             className="my-8 md:grid md:grid-cols-4 md:items-baseline"
@@ -32,7 +39,9 @@ export default function Posts() {
               <Link href={post.slug}>
                 <h2 className="font-bold text-xl mb-1">{post.title}</h2>
                 {post.description && (
-                  <p className="text-zinc-600 dark:text-zinc-400">{post.description}</p>
+                  <p className="text-zinc-600 dark:text-zinc-400">
+                    {post.description}
+                  </p>
                 )}
               </Link>
             </div>
